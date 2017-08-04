8f1065de24a3e0019da837aa3360197c84550b26
polygeekism <mcmahon.kevin.t@gmail.com>
2017-08-04 16:49:15 -0500

    button listeners successfully logging

--- public/client.js
+++ public/client.js
@@ -1,6 +1,20 @@
 console.log('script.js is sourced');
 
-$(document).ready(function(){
+$(document).ready(function () {
     console.log('jquery is soured');
+
+    $('#buttonContainer').on('click', '#addition', function () {
+        console.log('Addition clicked');
+
+    });
+    $('#buttonContainer').on('click', '#subtraction', function () {
+        console.log('Subtraction clicked')
+    });
+    $('#buttonContainer').on('click', '#multiplication', function () {
+        console.log('Multiplication clicked')
+    });
+    $('#buttonContainer').on('click', '#division', function () {
+        console.log('Division clicked')
+    });
 
 });
