7dc5fc4d35b18d5a2408106ddb30b3f5a32fc1b9
polygeekism <mcmahon.kevin.t@gmail.com>
2017-08-05 21:52:33 -0500

    got one post to work for the add button. Returning a value from the server.

--- app.js
+++ app.js
@@ -12,3 +12,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.listen(port, function(){
     console.log('app listening on ', port);
 });
+
+app.post('/addition', function(req, res){
+    console.log(req.body);
+    var sum = parseInt(req.body.x) + parseInt(req.body.y);
+    console.log('the sum is' + sum);
+    res.send(sum.toString());
+});
--- public/client.js
+++ public/client.js
@@ -2,19 +2,57 @@ console.log('script.js is sourced');
 
 $(document).ready(function () {
     console.log('jquery is soured');
+    
 
     $('#buttonContainer').on('click', '#addition', function () {
-        console.log('Addition clicked');
-
+        //console.log('Addition clicked');
+        var addObject = mathObjectBuilder('add');
+        console.log(addObject);
+        $.ajax({
+            method: 'POST',
+            url: '/addition',
+            data: addObject,
+            success: function(response){
+                console.log(response);
+            }
+        });
+        
     });
     $('#buttonContainer').on('click', '#subtraction', function () {
-        console.log('Subtraction clicked')
+        //console.log('Subtraction clicked')
+        mathObjectBuilder('subtract');
     });
     $('#buttonContainer').on('click', '#multiplication', function () {
-        console.log('Multiplication clicked')
+        //console.log('Multiplication clicked')
+        mathObjectBuilder('multiply');
     });
     $('#buttonContainer').on('click', '#division', function () {
-        console.log('Division clicked')
+        //console.log('Division clicked')
+        mathObjectBuilder('divide');
+    });
+    $('#clearButton').on('click', function () {
+        console.log('clear button clicked');
+        clearPage();
     });
 
+    
+    //ajax function
 });
+//function that takes operator and builds object
+function mathObjectBuilder(operator) {
+    var mathObject = {
+        x: $('#firstNumber').val(),
+        y: $('#secondNumber').val(),
+        type: operator
+    }
+    //eventually this will send mahtobject to an ajax post
+    return mathObject;
+}
+//some kind of ajax post  
+
+function clearPage() {
+        $('#firstNumber').val('');
+        $('#secondNumber').val('');
+        $('#displayResults').empty();
+
+    }
